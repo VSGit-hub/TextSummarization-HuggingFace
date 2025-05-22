744d301f62d37ac5e6916b5d60ee3d226ad93ed0
Vaibhav-Shinde <vbs.codestuff@gmail.com>
2025-05-22 10:12:34 +0530

    completed API for predict

--- main.py
+++ main.py
@@ -1,26 +1,51 @@
 from src.textsummarizer.logging import logger
 from src.textsummarizer.pipeline.stage_1_data_ingestion_pipeline import DataIngestionTrainingPipeline
 from src.textsummarizer.pipeline.stage_2_data_transformation_pipeline import DataIngestionTransformationPipeline
+from src.textsummarizer.pipeline.stage_3_model_trainer_pipeline import ModelTrainerPipeline
+from src.textsummarizer.pipeline.steage_4_model_evaluation_piepline import ModelEvaluationTrainingPipeline
 
-STAGE_NAME="DATA INGESTION STAGE"
+# STAGE_NAME="DATA INGESTION STAGE"
 
-try:
-    logger.info(f"stage {STAGE_NAME} initiated")
-    data_ingestion_pipeline=DataIngestionTrainingPipeline()
-    data_ingestion_pipeline.initiate_data_ingestion()
-    logger.info(f"Stage {STAGE_NAME} Completed")
-except Exception as e:
-    logger.exception(e)
-    raise e
+# try:
+#     logger.info(f"stage {STAGE_NAME} initiated")
+#     data_ingestion_pipeline=DataIngestionTrainingPipeline()
+#     data_ingestion_pipeline.initiate_data_ingestion()
+#     logger.info(f"Stage {STAGE_NAME} Completed")
+# except Exception as e:
+#     logger.exception(e)
+#     raise e
 
 
-STAGE_NAME="DATA TRANSFORMATION STAGE"
+# STAGE_NAME="DATA TRANSFORMATION STAGE"
 
+# try:
+#     logger.info(f"STAGE {STAGE_NAME} STARTED")
+#     data_transformation_pipeline = DataIngestionTransformationPipeline()
+#     data_transformation_pipeline.initiate_data_tranformation()
+#     logger.info(f"STAGE {STAGE_NAME} COMPLETED")
+# except Exception as e:
+#     logger.exception(e)
+#     raise e
+
+
+# STAGE_NAME="MODEL TRAINING STAGE"
+
+# try:
+#     logger.info(f"STAGE {STAGE_NAME} STARTED")
+#     model_trainer = ModelTrainerPipeline()
+#     model_trainer.initiate_model_trainer()
+#     logger.info(f"STAGE {STAGE_NAME} COMPLETED")
+# except Exception as e:
+#     logger.exception(e)
+#     raise e
+
+
+STAGE_NAME = "MODEL EVALUATION STAGE"
 try:
     logger.info(f"STAGE {STAGE_NAME} STARTED")
-    data_transformation_pipeline = DataIngestionTransformationPipeline()
-    data_transformation_pipeline.initiate_data_tranformation()
+    model_evaluation = ModelEvaluationTrainingPipeline()
+    model_evaluation.initiate_model_evaluation()
     logger.info(f"STAGE {STAGE_NAME} COMPLETED")
 except Exception as e:
-    logger.exception(e)
-    raise e
+        logger.exception(e)
+        raise e
